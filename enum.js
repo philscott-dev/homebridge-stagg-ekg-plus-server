53f9c8a36363dca3c971d5cba1f3174bd5c170d6
philscott-dev <phliscott.dev@gmail.com>
2020-10-04 22:18:54 +0100

    package-lock

--- enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PowerState = void 0;
-var PowerState;
-(function (PowerState) {
-    PowerState[PowerState["Off"] = 0] = "Off";
-    PowerState[PowerState["On"] = 1] = "On";
-})(PowerState = exports.PowerState || (exports.PowerState = {}));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZW51bS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInNyYy9lbnVtLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLElBQVksVUFHWDtBQUhELFdBQVksVUFBVTtJQUNwQix5Q0FBRyxDQUFBO0lBQ0gsdUNBQUUsQ0FBQTtBQUNKLENBQUMsRUFIVyxVQUFVLEdBQVYsa0JBQVUsS0FBVixrQkFBVSxRQUdyQiJ9
